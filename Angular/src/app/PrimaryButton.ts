c596700595b7fd32f22b8f23b1f27fe6bc0d7cab
Si2001lorenzo.gaggero <gaggero.lorenzo@gmail.com>
2021-04-12 12:44:51 +0200

    first commit

---
+++ Angular/src/app/PrimaryButton.ts
@@ -0,0 +1,7 @@
+import {MyButtonConfig} from './my-button-config';
+
+export const PrimaryButton: MyButtonConfig = {
+  customCssClass: 'btn btn-primary',
+  text: 'Primary',
+  icon: 'star_outline'
+};
