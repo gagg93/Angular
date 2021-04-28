d243877620eca505bcf0bf2c6fea6339d577eab3
Si2001lorenzo.gaggero <gaggero.lorenzo@gmail.com>
2021-04-28 12:23:50 +0200

    first commit

---
+++ bootproj/src/main/java/com/si2001/bootproj/BootprojApplication.java
@@ -0,0 +1,13 @@
+package com.si2001.bootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootprojApplication.class, args);
+    }
+
+}
---
+++ bootproj/src/test/java/com/si2001/bootproj/BootprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.si2001.bootproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
